9b14d4dd2ee4b35a1c1503881069fe931d501946
AlexisD98 <71796251+AlexisD98@users.noreply.github.com>
2025-02-24 16:41:20 -0600

    Update Hola.java

--- Hola.java
+++ Hola.java
@@ -1,5 +1,7 @@
 public class Hola{
     public static void main (String[]  args){
+        /*La linea 5 es una linea de codigo para poder imprimir en pantalla y que el usuario
+        pueda ver el resultado*/
         System.out.println("Hola Mundo");
     }
 }
